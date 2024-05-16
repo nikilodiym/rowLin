bd888a51833abfca407c5e7b5d6fa2a1b483bb9f
Nikita Krenov <nikitabaram@gmail.com>
2024-05-16 18:57:31 +0200

    The user enters two range limits and a number from the keyboard. If the number does not fall within the range, the program prompts the user to re-enter the number, and so on until they enter the number correctly.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,26 @@ using namespace std;
 
 
 int main() {
-	
+	int a, b, num;
+	cout << "Enter first numbers: ";
+	cin >> a;
+	cout << "Enter second numbers: ";
+	cin >> b;
+
+	if (a >= b) {
+		cout << "Error(( firts number must be greater than the second!!" << endl;
+		cin >> num;
+	}
+	cout << "Enter the number: ";
+	cin >> num;
+
+	while (num < a || num > b) {
+		cout << "This number is out of range. Please try again!" << endl;
+		cout << "Enter the new number: ";
+		cin >> num;
+	}
+
+	cout << "Nice job, this number in range!     Range [" << a << ", " << b << "]" << endl;
 
 	system("pause");
 	return 0;
