5667d2c5a75e1b15fafe33da1a9db3a5e3a201f2
Nikita Krenov <nikitabaram@gmail.com>
2024-05-18 22:16:45 +0200

    уч5

--- main.cpp
+++ main.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-
 int main() {
-	
+    int size = 5;
 
-	system("pause");
-	return 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (j < i) {
+                cout << "  ";
+            }
+            else {
+                cout << "* ";
+            }
+        }
+        for (int j = 0; j < size; j++) {
+            if (j < size - i - 1) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    for (int i = 1; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (j < size - i - 1) {
+                cout << "  ";
+            }
+            else {
+                cout << "* ";
+            }
+        }
+        for (int j = 0; j < size; j++) {
+            if (j < i) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    system("pause");
+    return 0;
 }
