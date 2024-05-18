1a2c5a0cc1927b8f0af3f5b052e3252ccbda08f5
Nikita Krenov <nikitabaram@gmail.com>
2024-05-18 17:46:12 +0300

    triangle3

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 
 int main() {
-	
+    int col = 5;
+    int row = 5;
+    for (size_t i = 0; i < col; i++)
+    {
+        for (size_t j = 0; j < row; j++)
+        {
+            if (j >= i && j < col - i) {
+                std::cout << "* ";
+            }
+            else {
+                std::cout << "  ";
+            }
+        }
+        cout << endl;
+    }
 
 	system("pause");
 	return 0;
