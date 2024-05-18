4af619df5e58507378628abe81ff19ee40bec4a2
Nikita Krenov <nikitabaram@gmail.com>
2024-05-18 21:21:33 +0200

    уч4

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 #include <string>
 using namespace std;
 
-
 int main() {
-	
+    int col = 5;
+    int row = 5;
+    for (size_t i = 0; i < col; i++)
+    {
+        for (size_t j = 0; j < row; j++)
+        {
+            if (j >= col - 1 - i && j <= i) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
 
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
